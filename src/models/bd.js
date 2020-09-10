e1175720dfc2002204fd80a527d2edfcf23a2db9
yoselin-19 <annelice119@gmail.com>
2020-09-10 17:22:55 -0600

    BD e imagenes

---
+++ src/models/bd.js
@@ -0,0 +1,33 @@
+export default bd = {
+    'animales': [
+        {"id": 1, "nombre":"pulpo", "nombre_cientifico": "Octopoda", "altura_longitud": "1", "peso": "10", "tiempo_vida": "5", "continente": "Asia, Europa, America, Oceania", "poblacion": "111555", "velocidad": "11.94", "img": "/img/pulpo.jpg"},
+        {"id": 2, "nombre":"calamar_gigante", "nombre_cientifico": "Teuthida", "altura_longitud": "0.4", "peso": "500", "tiempo_vida": "30", "continente": "Europa Oceania, Asia", "poblacion": "123456", "velocidad": "8.33", "img": "/img/calamar.JPG"},
+        {"id": 3, "nombre":"sepia", "nombre_cientifico": "Sepia officinalis", "altura_longitud": "0.4", "peso": "2", "tiempo_vida": "2", "continente": "Africa, Oceania", "poblacion": "1225465", "velocidad": "2.78", "img": "/img/sepia.JPG"},
+        {"id": 4, "nombre":"nautilus", "nombre_cientifico": "Nautilus pompilius", "altura_longitud": "0.2", "peso": "0.4", "tiempo_vida": "20", "continente": "Asia", "poblacion": "2456789", "velocidad": "2.78", "img": "/img/nautilus.jpg"},
+        {"id": 5, "nombre":"bivalvos", "nombre_cientifico": "Bivalvia", "altura_longitud": "1.40", "peso": "330", "tiempo_vida": "30", "continente": "Asia, Oceania", "poblacion": "4456781", "velocidad": "2.78", "img": "/img/bivalvo.jpg"},
+
+        {"id": 6, "nombre":"cometa", "nombre_cientifico": "Carassius auratus auratus", "altura_longitud": "0.2", "peso": "0.5", "tiempo_vida": "10", "continente": "America", "poblacion": "6245678", "velocidad": "51.39", "img": "/img/cometa.jpg"},
+        {"id": 7, "nombre":"beta", "nombre_cientifico": "Betta splendens", "altura_longitud": "0.07", "peso": "0.01", "tiempo_vida": "5", "continente": "Asia", "poblacion": "754877", "velocidad": "2.78", "img": "/img/beta.jpg"},
+        {"id": 8, "nombre":"carpa_comun", "nombre_cientifico": "Cyprinus carpio", "altura_longitud": "0.80", "peso": "14", "tiempo_vida": "20", "continente": "Asi", "poblacion": "8554724", "velocidad": "2.78", "img": "/img/carpa.jpg"},
+        {"id": 9, "nombre":"oranda", "nombre_cientifico": "Carassius auratus auratus", "altura_longitud": "0.31", "peso": "0.05", "tiempo_vida": "50", "continente": "Asia, Oceania, America", "poblacion": "42254781", "velocidad": "2.78", "img": "/img/oranda.jpg"},
+        {"id": 10, "nombre":"espada", "nombre_cientifico": "Xiphias gladius", "altura_longitud": "4.5", "peso": "500", "tiempo_vida": "10", "continente": "America, Asia", "poblacion": "333285", "velocidad": "27.78", "img": "/img/espada.jpg"},
+
+        {"id": 11, "nombre":"iguana", "nombre_cientifico": "Iguana", "altura_longitud": "0.42", "peso": "4", "tiempo_vida": "15", "continente": "America", "poblacion": "4000", "velocidad": "9.72", "img": "/img/iguana.jpg"},
+        {"id": 12, "nombre":"lagarto", "nombre_cientifico": "Lacertilia", "altura_longitud": "0.75", "peso": "0.5", "tiempo_vida": "20", "continente": "America, Oceania, Africa", "poblacion": "100330", "velocidad": "1.5", "img": "/img/lagarto.png"},
+        {"id": 13, "nombre":"cocodrilo", "nombre_cientifico": "Crocodylidae", "altura_longitud": "5.2", "peso": "1000", "tiempo_vida": "100", "continente": "Africa, America, Asia", "poblacion": "122554", "velocidad": "8.06", "img": "/img/cocodrilo.jpg"},
+        {"id": 14, "nombre":"tortuga", "nombre_cientifico": "Testudines", "altura_longitud": "2.2", "peso": "700", "tiempo_vida": "80", "continente": "Africa, America, Asia, Europa, Oceania", "poblacion": "5224501", "velocidad": "2.78", "img": "/img/tortuga.jpg"},
+        {"id": 15, "nombre":"camaleon", "nombre_cientifico": "Squamata", "altura_longitud": "0.51", "peso": "0.5", "tiempo_vida": "5", "continente": "Africa, America", "poblacion": "161000", "velocidad": "9.72", "img": "/img/camaleon.jpg"},
+
+        {"id": 16, "nombre":"garza", "nombre_cientifico": "Ardeidae", "altura_longitud": "1.5", "peso": "1.5", "tiempo_vida": "15", "continente": "Africa, America, Asia, Europa, Oceania", "poblacion": "522426", "velocidad": "17.78", "img": "/img/garza.jpg"},
+        {"id": 17, "nombre":"aguila", "nombre_cientifico": "Aquila chrysaetos", "altura_longitud": "100", "peso": "6.3", "tiempo_vida": "20", "continente": "America, Oceania", "poblacion": "225471", "velocidad": "44.44", "img": "/img/aguila.jpg"},
+        {"id": 18, "nombre":"codorniz", "nombre_cientifico": "Coturnix coturnix", "altura_longitud": "0.18", "peso": "0.096", "tiempo_vida": "2", "continente": "Africa, America, Asia, Europa, Oceania, Antartida", "poblacion": "125478", "velocidad": "16.67", "img": "/img/codorniz.jpg"},
+        {"id": 19, "nombre":"tucan", "nombre_cientifico": "Ramphastidae", "altura_longitud": "0.61", "peso": "0.620", "tiempo_vida": "20", "continente": "America", "poblacion": "1222645", "velocidad": "17.78", "img": "/img/tucan.jpg"},
+        {"id": 20, "nombre":"colibri", "nombre_cientifico": "Trochilidae", "altura_longitud": "0.09", "peso": "0.0031", "tiempo_vida": "5", "continente": "America", "poblacion": "330000", "velocidad": "21.94", "img": "/img/colibri.jpg"},
+
+        {"id": 21, "nombre":"panda", "nombre_cientifico": "Ailuropoda melanoleuca", "altura_longitud": "90", "peso": "100", "tiempo_vida": "20", "continente": "Asia", "poblacion": "1864", "velocidad": "0.006", "img": "/img/panda.jpg"},
+        {"id": 22, "nombre":"tigre", "nombre_cientifico": "Panthera tigris", "altura_longitud": "3.9", "peso": "310", "tiempo_vida": "15", "continente": "Asia", "poblacion": "2226", "velocidad": "18.06", "img": "/img/tigre.png"},
+        {"id": 23, "nombre":"koala", "nombre_cientifico": "Phascolarctos cinereus", "altura_longitud": "0.85", "peso": "15", "tiempo_vida": "18", "continente": "Oceania", "poblacion": "80000", "velocidad": "5.56", "img": "/img/koala.jpg"},
+        {"id": 24, "nombre":"leopardo", "nombre_cientifico": "Panthera pardus", "altura_longitud": "1.60", "peso": "31", "tiempo_vida": "17", "continente": "Africa, Asia", "poblacion": "2000", "velocidad": "16.11", "img": "/img/leopardo.jpg"},
+        {"id": 25, "nombre":"elefante", "nombre_cientifico": "Elephantidae", "altura_longitud": "3.2", "peso": "6000", "tiempo_vida": "70", "continente": "Africa, Asia", "poblacion": "63000", "velocidad": "6.94", "img": "/img/elefante.jpg"}
+    ]
+}
