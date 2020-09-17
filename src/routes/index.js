100c7efb3b73f4fccd0f8bfc16cb3fea9db75939
Ruben Osorio <rubenosorio88@gmail.com>
2020-09-17 02:32:47 -0600

    se obtienen y se presentan los datos

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const pl = require('tau-prolog');
 
 var session = pl.create( 1000 );
+var animales = [];
 const router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -13,52 +14,77 @@ router.get('/animal/:id', (req, res) => {
     res.render("pages/animal")
 });
 
-router.post('/consultar', (req, rest) => {
+router.post('/consultar', (req, res) => {
     var program =
-    `animal(molusco, pulpo,           invertebrado, fria,       no,   no,  respirar, marino,   oviparo,   no,    no,     no, siente, vida, baja,  no,         grande, normal).
-    animal(molusco, calamar_gigante, invertebrado, fria,        no,   no,   respirar, marino,   oviparo,    no,    no,     no, siente, vida, media, vulnerable, grande, normal).
-    animal(molusco, sepia,           invertebrado, fria,        no,   no,   respirar, terrestre,oviparo,    no,    no,     no, siente, vida, baja,  no,         pequenio, lento).
-    animal(molusco, nautilus,        invertebrado, fria,        no,   no,   respirar, marino,   oviparo,    no,    no,     no, siente, vida, alta,  vulnerable, pequenio, lento).
-    animal(molusco, bivalvos,        invertebrado, fria,        no,   no,   respirar, terrestre,oviparo,    no,    no,     no, siente, vida, baja,  no,         pequenio, lento).
+    `animal(id(1), molusco, pulpo,           invertebrado, fria,       no,   no,  respirar, marino,   oviparo,   no,    no,     no, siente, vida, baja,  no,         grande, normal).
+    animal(id(2), molusco, calamar_gigante, invertebrado, fria,        no,   no,   respirar, marino,   oviparo,    no,    no,     no, siente, vida, media, vulnerable, grande, normal).
+    animal(id(3), molusco, sepia,           invertebrado, fria,        no,   no,   respirar, terrestre,oviparo,    no,    no,     no, siente, vida, baja,  no,         pequenio, lento).
+    animal(id(4), molusco, nautilus,        invertebrado, fria,        no,   no,   respirar, marino,   oviparo,    no,    no,     no, siente, vida, alta,  vulnerable, pequenio, lento).
+    animal(id(5), molusco, bivalvos,        invertebrado, fria,        no,   no,   respirar, terrestre,oviparo,    no,    no,     no, siente, vida, baja,  no,         pequenio, lento).
     
-    animal(pez, cometa,              vertebrado, fria,       escamas, no,   respirar, acuatico, oviparo,    no,    no, huesos, siente, vida, baja,      no,     pequenio, rapido).
-    animal(pez, beta,                vertebrado, fria,       escamas, no,   respirar, acuatico, oviparo,    no,    no, huesos, siente, vida, baja,      no,     pequenio, rapido).
-    animal(pez, carpa_comun,         vertebrado, fria,       escamas, no,   respirar, acuatico, oviparo,    no,    no, huesos, siente, vida, baja,      no,     pequenio, rapido).
-    animal(pez, oranda,              vertebrado, fria,       escamas, no,   respirar, acuatico, oviparo,    no,    no, huesos, siente, vida, baja,      no,     pequenio, rapido).
-    animal(pez, espada,              vertebrado, fria,       escamas, no,   respirar, acuatico, oviparo,    no,    no, huesos, siente, vida, baja,      no,     pequenio, rapido).
+    animal(id(6), pez, cometa,              vertebrado, fria,       escamas, no,   respirar, acuatico, oviparo,    no,    no, huesos, siente, vida, baja,      no,     pequenio, rapido).
+    animal(id(7), pez, beta,                vertebrado, fria,       escamas, no,   respirar, acuatico, oviparo,    no,    no, huesos, siente, vida, baja,      no,     pequenio, rapido).
+    animal(id(8), pez, carpa_comun,         vertebrado, fria,       escamas, no,   respirar, acuatico, oviparo,    no,    no, huesos, siente, vida, baja,      no,     pequenio, rapido).
+    animal(id(9), pez, oranda,              vertebrado, fria,       escamas, no,   respirar, acuatico, oviparo,    no,    no, huesos, siente, vida, baja,      no,     pequenio, rapido).
+    animal(id(10), pez, espada,              vertebrado, fria,       escamas, no,   respirar, acuatico, oviparo,    no,    no, huesos, siente, vida, baja,      no,     pequenio, rapido).
     
-    animal(reptil, iguana,           vertebrado, fria,       escamas, no,   respirar, terrestre,oviparo,    no,    no, huesos, siente, vida, media,     no,     pequenio, rapido).
-    animal(reptil, lagarto,          vertebrado, fria,       escamas, no,   respirar, terrestre,oviparo,    no,    no, huesos, siente, vida, media,     no,     pequenio, rapido).
-    animal(reptil, cocodrilo,        vertebrado, fria,       escamas, no,   respirar, terrestre,oviparo,    no,    no, huesos, siente, vida, alta, vulnerable,  grande,   normal).
-    animal(reptil, tortuga,          vertebrado, fria,       escamas, no,   respirar, terrestre,oviparo,    no,    no, huesos, siente, vida, alta, vulnerable,  pequenio, lento).
-    animal(reptil, camaleon,         vertebrado, fria,       escamas, no,   respirar, terrestre,oviparo,    no,    no, huesos, siente, vida, baja, vulnerable,  pequenio, lento).
+    animal(id(11), reptil, iguana,           vertebrado, fria,       escamas, no,   respirar, terrestre,oviparo,    no,    no, huesos, siente, vida, media,     no,     pequenio, rapido).
+    animal(id(12), reptil, lagarto,          vertebrado, fria,       escamas, no,   respirar, terrestre,oviparo,    no,    no, huesos, siente, vida, media,     no,     pequenio, rapido).
+    animal(id(13), reptil, cocodrilo,        vertebrado, fria,       escamas, no,   respirar, terrestre,oviparo,    no,    no, huesos, siente, vida, alta, vulnerable,  grande,   normal).
+    animal(id(14), reptil, tortuga,          vertebrado, fria,       escamas, no,   respirar, terrestre,oviparo,    no,    no, huesos, siente, vida, alta, vulnerable,  pequenio, lento).
+    animal(id(15), reptil, camaleon,         vertebrado, fria,       escamas, no,   respirar, terrestre,oviparo,    no,    no, huesos, siente, vida, baja, vulnerable,  pequenio, lento).
     
-    animal(ave, codorniz,            vertebrado, caliente,   plumas,  alas, respirar, terrestre,oviparo,    no, volar, huesos, siente, vida, media,    no,      pequenio, rapido).
-    animal(ave, garza,               vertebrado, caliente,   plumas,  alas, respirar, terrestre,oviparo,    no, volar, huesos, siente, vida, media,    no,      pequenio, rapido).
-    animal(ave, aguila,              vertebrado, caliente,   plumas,  alas, respirar, terrestre,oviparo,    no, volar, huesos, siente, vida, media,    no,      pequenio, rapido).
-    animal(ave, tucan,               vertebrado, caliente,   plumas,  alas, respirar, terrestre,oviparo,    no, volar, huesos, siente, vida, media,    no,      pequenio, rapido).
-    animal(ave, colibri,             vertebrado, caliente,   plumas,  alas, respirar, terrestre,oviparo,    no, volar, huesos, siente, vida, baja,     no,      pequenio, rapido).
+    animal(id(16), ave, codorniz,            vertebrado, caliente,   plumas,  alas, respirar, terrestre,oviparo,    no, volar, huesos, siente, vida, media,    no,      pequenio, rapido).
+    animal(id(17), ave, garza,               vertebrado, caliente,   plumas,  alas, respirar, terrestre,oviparo,    no, volar, huesos, siente, vida, media,    no,      pequenio, rapido).
+    animal(id(18), ave, aguila,              vertebrado, caliente,   plumas,  alas, respirar, terrestre,oviparo,    no, volar, huesos, siente, vida, media,    no,      pequenio, rapido).
+    animal(id(19), ave, tucan,               vertebrado, caliente,   plumas,  alas, respirar, terrestre,oviparo,    no, volar, huesos, siente, vida, media,    no,      pequenio, rapido).
+    animal(id(20), ave, colibri,             vertebrado, caliente,   plumas,  alas, respirar, terrestre,oviparo,    no, volar, huesos, siente, vida, baja,     no,      pequenio, rapido).
     
-    animal(mamifero, panda,          vertebrado, caliente,   pelo,    no,   respirar, terrestre,    no,   leche,  no,  huesos, siente, vida, media, vulnerable, grande,   lento).
-    animal(mamifero, koala,          vertebrado, caliente,   pelo,    no,   respirar, terrestre,    no,   leche,  no,  huesos, siente, vida, media, vulnerable, pequenio, lento).
-    animal(mamifero, leopardo,       vertebrado, caliente,   pelo,    no,   respirar, terrestre,    no,   leche,  no,  huesos, siente, vida, media, vulnerable, grande,   rapido).
-    animal(mamifero, tigre,          vertebrado, caliente,   pelo,    no,   respirar, terrestre,    no,   leche,  no,  huesos, siente, vida, media, vulnerable, grande,   rapido).
-    animal(mamifero, elefante,       vertebrado, caliente,   pelo,    no,   respirar, terrestre,    no,   leche,  no,  huesos, siente, vida, alta,  vulnerable, grande,   normal).
+    animal(id(21), mamifero, panda,          vertebrado, caliente,   pelo,    no,   respirar, terrestre,    no,   leche,  no,  huesos, siente, vida, media, vulnerable, grande,   lento).
+    animal(id(22), mamifero, koala,          vertebrado, caliente,   pelo,    no,   respirar, terrestre,    no,   leche,  no,  huesos, siente, vida, media, vulnerable, pequenio, lento).
+    animal(id(23), mamifero, leopardo,       vertebrado, caliente,   pelo,    no,   respirar, terrestre,    no,   leche,  no,  huesos, siente, vida, media, vulnerable, grande,   rapido).
+    animal(id(24), mamifero, tigre,          vertebrado, caliente,   pelo,    no,   respirar, terrestre,    no,   leche,  no,  huesos, siente, vida, media, vulnerable, grande,   rapido).
+    animal(id(25), mamifero, elefante,       vertebrado, caliente,   pelo,    no,   respirar, terrestre,    no,   leche,  no,  huesos, siente, vida, alta,  vulnerable, grande,   normal).
             1           2               3           4           5       6       7           8       9       10     11     12    13      14      15      16          17      18 `;
     //armar query
                         //1     2       3       4           5       6           7       8           9       10      11      12      13          14      15          16          17      18    
-    let query = "animal(@tipo, X, @vertebrado, @sangre, @exterior, @alas, @respira, @territorio, @oviparo, @leche, @volar, @huesos, @siente, @vida, @longevidad, @peligro, @tamanio, @velocidad ).";
+    let query = "animal(id(Y), @tipo, X, @vertebrado, @sangre, @exterior, @alas, @respira, @territorio, @oviparo, @leche, @volar, @huesos, @siente, @vida, @longevidad, @peligro, @tamanio, @velocidad ).";
     //
     const obj = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { title: 'product' }
     console.log(obj);
     query = getLongevidad(obj, query);
     query = cleanQuery(query);
-    console.log(query);
+
     session.consult( program );
     session.query( query);
-    session.answers( x => console.log( pl.format_answer(x) ) );
-
-
+    
+   // session.answers( x => console.log( pl.format_answer(x) ) );
+   var count_answers = 0
+    var callback = function(answer) {
+        if (answer === false) {
+         
+          console.log('DONE, #answers='+count_answers)
+          return
+        }
+        if (answer === null) {
+          console.log('TIMEOUT, #answers='+count_answers)
+          return
+        }
+        // loop
+        ++count_answers
+        var respuesta = pl.format_answer(answer);
+        var animal = {};
+        try{
+            animal = {"id":respuesta.split(",")[0].split("=")[1].trim(), "animal": respuesta.split(",")[1].split("=")[1].replace(";", "").trim()}
+            animales.push(animal);
+         }catch(e){}
+        
+        session.answer(callback)
+      }
+      // start the query loop
+      session.answer(callback)
+      res.send(animales.toString());
+      
 });
 
 function getLongevidad(obj, query){   
